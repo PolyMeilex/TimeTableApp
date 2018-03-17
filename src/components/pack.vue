46f91a093b5dd00a7ed6d8c07ce3b4ad9ad6b450
PolyMagic <bartek.marynczak@op.pl>
2018-03-17 18:48:40 +0100

    pack component

---
+++ src/components/pack.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+
+<script>
+export default {
+  data(){
+    zas:1
+  },
+  methods:{
+    a(){
+      console.log("tst");
+    }
+
+  }
+}
+</script>
--- src/router.js
+++ src/router.js
@@ -29,6 +29,7 @@ export default new VueRouter({
     { path: '/mainA1', component: load('mainA1') },
     { path: '/info', component: load('info') },
     { path: '/zasts', component: load('zasts') },
+    { path: '/pack', component: load('pack') },
     // Always leave this last one
     { path: '*', component: load('Hello') } // Not found
   ]
