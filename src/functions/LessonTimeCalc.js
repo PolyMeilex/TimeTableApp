f80beaaeb13c9e0902687b63e792c49ed42f67d5
PolyMagic <bartek.marynczak@op.pl>
2019-09-05 16:21:03 +0200

    10st lesson fix

--- src/functions/LessonTimeCalc.js
+++ src/functions/LessonTimeCalc.js
@@ -7,33 +7,24 @@ exports.GetLessonsInHour = date => {
 
   if (h < 8) {
     return { date, me: 0, x1: 1, x2: 1 };
-  }
-  else if (h == 8) {
+  } else if (h == 8) {
     return { date, me: 45, x1: 1, x2: 2 };
-  }
-  else if (h == 9) {
+  } else if (h == 9) {
     return { date, me: 40, x1: 2, x2: 3 };
-  }
-  else if (h == 10) {
+  } else if (h == 10) {
     return { date, me: 35, x1: 3, x2: 4 };
-  }
-  else if (h == 11) {
+  } else if (h == 11) {
     return { date, me: 30, x1: 4, x2: 5 };
-  }
-  else if (h == 12) {
+  } else if (h == 12) {
     return { date, me: 25, x1: 5, x2: 6 };
-  }
-  else if (h == 13) {
+  } else if (h == 13) {
     return { date, me: 20, x1: 6, x2: 7 };
-  }
-  else if (h == 14) {
+  } else if (h == 14) {
     return { date, me: 20, x1: 7, x2: 8 };
-  }
-  else if (h == 15) {
+  } else if (h == 15) {
     return { date, me: 15, x1: 8, x2: 9 };
-  }
-  else if (h == 16) {
-    return { date, me: 5, x1: 9, x2: 1 };
+  } else if (h == 16) {
+    return { date, me: 5, x1: 9, x2: 10 };
   }
   return { date, me: 0, x1: 1, x2: 1 };
 };
@@ -43,15 +34,14 @@ exports.GetLessonId = hourObj => {
   const me = hourObj.me; // End Minute
 
   if (m < me) {
-    return hourObj.x1
+    return hourObj.x1;
+  } else if (m >= me) {
+    return hourObj.x2;
   }
-  else if (m >= me) {
-    return hourObj.x2
-  }
-}
+};
 
 exports.QuickGetLessonId = date => {
   const hourObj = exports.GetLessonsInHour(date);
   const lessonId = exports.GetLessonId(hourObj);
   return lessonId;
-}
+};
