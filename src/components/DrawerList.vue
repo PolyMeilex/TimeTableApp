412ed8baa3bedbc2450701fc9512b22ec94df837
Poly <bartek.marynczak@op.pl>
2020-03-22 00:46:12 +0100

    Drawer Right

---
+++ src/components/DrawerList.vue
@@ -0,0 +1,62 @@
+<template>
+  <q-list>
+    <q-item-label header>Menu</q-item-label>
+
+    <q-item clickable @click="$router.push('/')">
+      <q-item-section avatar>
+        <q-icon name="schedule" />
+      </q-item-section>
+
+      <q-item-section>
+        <q-item-label>Bieżące Lekcje</q-item-label>
+        <q-item-label caption>Aktualna i przyszła lekcja</q-item-label>
+      </q-item-section>
+    </q-item>
+
+    <q-item clickable @click="$router.push('/FullTable')">
+      <q-item-section avatar>
+        <q-icon name="list" />
+      </q-item-section>
+
+      <q-item-section>
+        <q-item-label>Pełen Plan</q-item-label>
+        <q-item-label caption>Wyświetla Plan Całego Tygodnia</q-item-label>
+      </q-item-section>
+    </q-item>
+
+    <q-item clickable @click="$router.push('/WeekTable')">
+      <q-item-section avatar>
+        <q-icon name="table_chart" />
+      </q-item-section>
+
+      <q-item-section>
+        <q-item-label>Tabela</q-item-label>
+        <q-item-label caption>Wyświetla Tabele Całego Tygodnia</q-item-label>
+      </q-item-section>
+    </q-item>
+
+    <q-item clickable @click="$router.push('/News')">
+      <q-item-section avatar>
+        <q-icon name="chrome_reader_mode" />
+      </q-item-section>
+
+      <q-item-section>
+        <q-item-label>Ogłoszenia</q-item-label>
+        <q-item-label caption
+          >Zastępstwa I Inne Ogłoszenia Ze Strony Szkoły</q-item-label
+        >
+      </q-item-section>
+    </q-item>
+
+    <q-item clickable @click="$router.push('/Settings')">
+      <q-item-section avatar>
+        <q-icon name="settings" />
+      </q-item-section>
+
+      <q-item-section>
+        <q-item-label>Ustawienia</q-item-label>
+        <q-item-label caption>Ustawienia użytkownika</q-item-label>
+      </q-item-section>
+    </q-item>
+  </q-list>
+</template>
--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -2,93 +2,48 @@
   <q-layout view="lHh Lpr lFf" style="background-color: rgb(23, 23, 23);">
     <q-header elevated>
       <q-toolbar class="bg-dark">
-        <q-btn flat dense round @click="leftDrawerOpen = !leftDrawerOpen" aria-label="Menu">
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+        >
           <q-icon name="menu" />
         </q-btn>
 
         <div class="toolbar-title">
-          Plan Lekcji {{userClassLabel}}
+          Plan Lekcji {{ userClassLabel }}
           <div class="toolbar-subtitle">
-            <div>v{{version}}</div>
+            <div>v{{ version }}</div>
           </div>
         </div>
 
         <q-btn
           flat
           round
-          @click="$emit('setGrp',1)"
-          :color="userGrp==1?'primary':'white'"
+          @click="$emit('setGrp', 1)"
+          :color="userGrp == 1 ? 'primary' : 'white'"
           icon="bookmark"
-        >1</q-btn>
+          >1</q-btn
+        >
         <q-btn
           flat
           round
-          @click="$emit('setGrp',2)"
-          :color="userGrp==2?'primary':'white'"
+          @click="$emit('setGrp', 2)"
+          :color="userGrp == 2 ? 'primary' : 'white'"
           icon="bookmark"
-        >2</q-btn>
+          >2</q-btn
+        >
       </q-toolbar>
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" bordered content-class="bg-grey-2">
-      <q-list>
-        <q-item-label header>Menu</q-item-label>
+      <drawer-list></drawer-list>
+    </q-drawer>
 
-        <q-item clickable @click="$router.push('/')">
-          <q-item-section avatar>
-            <q-icon name="schedule" />
-          </q-item-section>
-
-          <q-item-section>
-            <q-item-label>Bieżące Lekcje</q-item-label>
-            <q-item-label caption>Aktualna i przyszła lekcja</q-item-label>
-          </q-item-section>
-        </q-item>
-
-        <q-item clickable @click="$router.push('/FullTable')">
-          <q-item-section avatar>
-            <q-icon name="list" />
-          </q-item-section>
-
-          <q-item-section>
-            <q-item-label>Pełen Plan</q-item-label>
-            <q-item-label caption>Wyświetla Plan Całego Tygodnia</q-item-label>
-          </q-item-section>
-        </q-item>
-
-        <q-item clickable @click="$router.push('/WeekTable')">
-          <q-item-section avatar>
-            <q-icon name="table_chart" />
-          </q-item-section>
-
-          <q-item-section>
-            <q-item-label>Tabela</q-item-label>
-            <q-item-label caption>Wyświetla Tabele Całego Tygodnia</q-item-label>
-          </q-item-section>
-        </q-item>
-
-        <q-item clickable @click="$router.push('/News')">
-          <q-item-section avatar>
-            <q-icon name="chrome_reader_mode" />
-          </q-item-section>
-
-          <q-item-section>
-            <q-item-label>Ogłoszenia</q-item-label>
-            <q-item-label caption>Zastępstwa I Inne Ogłoszenia Ze Strony Szkoły</q-item-label>
-          </q-item-section>
-        </q-item>
-
-        <q-item clickable @click="$router.push('/Settings')">
-          <q-item-section avatar>
-            <q-icon name="settings" />
-          </q-item-section>
-
-          <q-item-section>
-            <q-item-label>Ustawienia</q-item-label>
-            <q-item-label caption>Ustawienia użytkownika</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
+    <q-drawer side="right" bordered content-class="bg-grey-2">
+      <drawer-list></drawer-list>
     </q-drawer>
 
     <q-page-container>
@@ -106,12 +61,16 @@
   </q-layout>
 </template>
 
-<script>
-import { version } from "../../package.json";
+<script lang="ts" lang="ts">
+import Vue from "vue";
 
-export default {
+import { version } from "../../package.json";
+import DrawerList from "../components/DrawerList.vue";
+
+export default Vue.extend({
   name: "MyLayout",
   props: ["userGrp", "userClassLabel", "plan", "forceReRender"],
+  components: { DrawerList },
   data() {
     return {
       version: version,
@@ -130,11 +89,11 @@ export default {
     }
   },
   created() {
-    if (this.$q.platform.is.desktop) {
-      this.leftDrawerOpen = true;
-    }
+    // if (this.$q.platform.is.desktop) {
+    // this.leftDrawerOpen = true;
+    // }
   }
-};
+});
 </script>
 
 <style>
